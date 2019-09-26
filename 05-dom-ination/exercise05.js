0f55c8ff1349d88f10ea2ee30e1afded24864b93
Anne Gunst <aegunst7@gmail.com>
2019-09-26 10:38:37 -0400

    add parts 4&5 in dom-ination

--- 05-dom-ination/exercise05.js
+++ 05-dom-ination/exercise05.js
@@ -23,11 +23,16 @@
     //your code here
     let feedTemplate = document.querySelector("h1");
     let sectionLinks = document.querySelectorAll(".title");
-    //let strArray = document.querySelectorAll("p");
-    //let str = "";
-    var postList = document.querySelectorAll(".post");
+    let strArray = document.querySelectorAll("p");
+    var str = "";
+    let postList = document.querySelectorAll(".post");
+    let ads = document.querySelector(".hide-for-small");
     var index;
-    // part 1
+    /* part 1:
+        Any time someone clicks on the title at the top "feed template,"
+        make the color of the "feed template" text change from black to red
+        and then from red to black when clicked again.
+    */
     feedTemplate.addEventListener("click", function() {
         if(feedTemplate.style.color == 'black') {
             feedTemplate.style.color = 'red';
@@ -37,12 +42,18 @@
         }
     });
 
-    // part 2
+    /* part 2:
+        The links on the side of the page – "your name," "section 1," "section 2," etc. –
+        hide every odd-numbered link in the menu.
+    */
     for (i = 0; i < sectionLinks.length; i += 2) {
         sectionLinks[i].style.visibility = 'hidden';
     }
 
-    // part 3
+    /* part 3:
+        Change every instance of the word "bacon" on the page to be "LASER VISION"
+    */
+    console.log(strArray);
     //for (i = 0; i < strArray.length; i++) {
   //  strArray[0].replace("bacon", "LASER VISION")
     //    console.log(strArray[i]);
@@ -54,34 +65,27 @@
     //curInnerHTML = curInnerHTML.replace("bacon", "LASER VISION");
     //document.body.innerHTML = curInnerHTML;
 
-    // part 4
-    /*
-    function removeElement(elementId) {
-        // Removes an element from the document
-        var element = document.getElementById(elementId);
-        element.parentNode.removeChild(element);
-    }
-    for (var i = list.length - 1; 0 <= i; i--) {
-        if (list[i] && list[i].parentElement) {
-            list[i].parentElement.removeChild(list[i]);
-        }
-    }
-
+    /* part 4
+        Delete the last two posts in the middle section (they have a CSS class "post")
     */
-    //removeBottomUp(2);
+    removeBottomUp(2);
     function removeBottomUp(numRemoved) {
         index = postList.length - 1;
 
         for (i=0;i<numRemoved; i++) {
-            console.log(index);
-            postList[index].parentElement.removeChild(postList[index]);
+            if (postList[i] && postList[i].parentElement) {
+                postList[i].parentElement.removeChild(postList[i]);
+            }
             index--;
-            console.log(index);
         }
         return;
     }
+
+    /* part 5
+        Remove the images in the right column
+    */
+    ads.remove();
     
-    
-   // list[i].parentElement.removeChild(list[list.length-1]);
+
 
 })();
